bc1f4359675abac60f1277aa9c4bf5edc67250ba
Omid H <hamadanyan@gmail.com>
2022-12-26 17:55:04 -0800

    code added to build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+cat templates/top.html + content/index.html + templates/bottom.html > docs/index.html
+cat templates/top.html + content/contact.html + templates/bottom.html > docs/contact.html
+cat templates/top.html + content/about.html + templates/bottom.html > docs/about.html
+cat templates/top.html + content/elements.html + templates/bottom.html > docs/elements.html
+cat templates/top.html + content/portfolio.html + templates/bottom.html > docs/portfolio.html
+cat templates/top.html + content/services.html + templates/bottom.html > docs/services.html
+
